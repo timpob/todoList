a55a2a74cfc01ddbfca4de9fac4bad3a9547ea84
Tymur Pobolov <timurpobolov2003@gmail.com>
2025-02-16 18:29:00 +0100

    seventh

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ root.render(
         <p>HELLO WORLD</p>
         <p>HELLO WORLD ONE MORE TIME</p>
         <p>HELLO WORLD ONE MORE TIME 2000</p>
-         <p>HELLO WORLD ONE MORE TIME 4000</p>
+        <p>HELLO WORLD ONE MORE TIME 4000</p>
         <p>HELLO WORLD ONE MORE TIME 6000</p>
+        <p>HELLO WORLD ONE MORE TIME 8000</p>
+
     </Provider>
 );
 
